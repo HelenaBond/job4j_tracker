ea2c40a08732321a65686e317346332c78bf6ca4
Elena <bond.elena.15.05@gmail.com>
2024-01-30 19:00:25 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker.checkstyle;
+
+public class Broken {
+    public static final String VALUE = "";
+
+    private int emptySize = 10;
+    private String surname;
+    private String name;
+
+    Broken() {
+
+    }
+
+    void echo() { }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+
+    }
+
+    void method(int a, int b, int c) {
+
+    }
+}
