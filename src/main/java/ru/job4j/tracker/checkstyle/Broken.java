150d0a47562383e73fd3221ba2fd57a3807e88da
Elena <bond.elena.15.05@gmail.com>
2024-01-31 09:21:01 +0300

    Refactoring. 6. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/tracker/checkstyle/Broken.java
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -7,20 +7,20 @@ public class Broken {
     private String surname;
     private String name;
 
-    Broken() {
+    public Broken() {
 
     }
 
-    void echo() { }
+    public void echo() { }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
 
     }
 
-    void method(int a, int b, int c) {
+    public void method(int a, int b, int c) {
 
     }
 }
