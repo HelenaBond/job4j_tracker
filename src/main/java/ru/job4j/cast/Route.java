78299591412b0ca9e8fa9bed4b2bf318b5d2c9d9
Elena <bond.elena.15.05@gmail.com>
2024-02-12 16:24:48 +0300

    Refactoring. 5.1. Зачем нужно приведение типов [#310019 #480754]

--- src/main/java/ru/job4j/cast/Route.java
+++ src/main/java/ru/job4j/cast/Route.java
@@ -2,16 +2,16 @@ package ru.job4j.cast;
 
 public class Route {
     public static void main(String[] args) {
-        Vehicle point1 = new Bus();
-        Vehicle point2 = new Train();
-        Vehicle point3 = new Airplane();
-        Vehicle point4 = new Bus();
-        Vehicle point5 = new Airplane();
-        Vehicle point6 = new Train();
-        Vehicle point7 = new Bus();
+        Vehicle start = new Bus();
+        Vehicle toMoscow = new Train();
+        Vehicle toAlmaty = new Airplane();
+        Vehicle transfer = new Bus();
+        Vehicle toRome = new Airplane();
+        Vehicle excursion = new Train();
+        Vehicle end = new Bus();
 
-        Vehicle[] moscowKazakhstanItaly = {point1, point2, point3, point4, point5, point6, point7};
-        for (Vehicle transport : moscowKazakhstanItaly) {
+        Vehicle[] weekend = {start, toMoscow, toAlmaty, transfer, toRome, excursion, end};
+        for (Vehicle transport : weekend) {
             transport.transportationPoint();
             transport.move();
         }
