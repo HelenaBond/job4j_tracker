b91b645144e5d573771094f49a6f24b041d057fd
Elena <bond.elena.15.05@gmail.com>
2024-02-15 14:47:57 +0300

    Refactoring. 12.1 Зависимость от System.out [#33568 #481340]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,22 +10,17 @@ public class StartUI {
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
-            showMenu();
+            showMenu(actions);
             int select = input.askInt("Выбрать: ");
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
     }
 
-    private void showMenu() {
-        String[] menu = {
-                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
-                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
-                "Завершить программу"
-        };
+    private void showMenu(UserAction[] actions) {
         output.println("Меню:");
-        for (int i = 0; i < menu.length; i++) {
-            output.println(i + ". " + menu[i]);
+        for (int i = 0; i < actions.length; i++) {
+            output.println(i + ". " + actions[i]);
         }
     }
 
