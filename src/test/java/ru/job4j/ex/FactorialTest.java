030c9d799b125d3fd5d100783bba71c8f3fe6828
Elena <bond.elena.15.05@gmail.com>
2024-02-26 11:45:57 +0300

    Refactoring. 0.3. Тестирование исключений с junit [#219366 #483302]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -14,4 +14,31 @@ class FactorialTest {
                 () -> factorial.calc(number));
         assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
     }
+
+    @Test
+    public void whenOneThenOne() {
+        int number = 1;
+        Factorial factorial = new Factorial();
+        int actual = factorial.calc(number);
+        int expected = 1;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenTwoThenTwo() {
+        int number = 2;
+        Factorial factorial = new Factorial();
+        int actual = factorial.calc(number);
+        int expected = 2;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenThreeThenSix() {
+        int number = 3;
+        Factorial factorial = new Factorial();
+        int actual = factorial.calc(number);
+        int expected = 6;
+        assertThat(actual).isEqualTo(expected);
+    }
 }
