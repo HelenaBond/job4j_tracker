57fad198af8de9b4b49ab0f365d9bfdbfe887899
Elena <bond.elena.15.05@gmail.com>
2024-05-12 11:52:14 +0300

    Refactoring. 4. Сортировка номера [#221284 #495576]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,8 +6,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        int leftNumber = Integer.parseInt(left.substring(0, left.indexOf(".")).trim());
-        int rightNumber = Integer.parseInt(right.substring(0, right.indexOf(".")).trim());
+        int leftNumber = Integer.parseInt(left.split("\\.")[0]);
+        int rightNumber = Integer.parseInt(right.split("\\.")[0]);
         return Integer.compare(leftNumber, rightNumber);
     }
 }
