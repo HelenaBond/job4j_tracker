664807344c3258fe2eb3b2fe770ba926bbb958a6
Elena <bond.elena.15.05@gmail.com>
2024-05-12 11:42:53 +0300

    Refactoring. 1. Организовать сортировку User [#10034 #495559]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -14,10 +14,11 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        if (this.name.compareTo(o.name) == 0) {
-            return Integer.compare(this.age, o.age);
+        int compareName = this.name.compareTo(o.name);
+        if (compareName == 0) {
+            compareName = Integer.compare(this.age, o.age);
         }
-        return this.name.compareTo(o.name);
+        return compareName;
     }
 
     @Override
