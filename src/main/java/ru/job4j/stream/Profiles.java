dbf46b97c361c47c2ea985adbfbb182f086618c2
Elena <bond.elena.15.05@gmail.com>
2024-05-24 14:23:40 +0300

    Refactoring 6. Список адресов [#110062 #497490]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -12,7 +12,7 @@ public class Profiles {
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         return profiles.stream()
-                .map(Profile :: getAddress)
+                .map(Profile::getAddress)
                 .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .toList();
